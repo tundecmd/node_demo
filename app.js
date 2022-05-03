6681660b3950094e06a7bfd9685537e22837598b
Adepoju Lateef <ademustexcel@gmail.com>
2022-05-03 13:15:58 +0100

    removed test script

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/api', (req, res) => {
+  res.json({
+    message: "Hello World !!!"
+  })
+})
 
 app.get('/', (req, res) => {
   res.send("Hello World!!!");
